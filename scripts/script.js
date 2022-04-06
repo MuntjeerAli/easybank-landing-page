89cb94b5c315a0d23676d535eb52367e418a9b0b
MuntjeerAli <muntjeersayyad@gmail.com>
2022-04-06 21:42:54 +0530

    Added Responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const btnToggle = document.querySelector('.btn-toggle');
+const navbar = document.querySelector('.navbar');
+
+btnToggle.addEventListener('click', () => {
+    navbar.classList.toggle('mobile');
+});
